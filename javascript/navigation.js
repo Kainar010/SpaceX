fb1fc6c80d2d0a77bed2fd19027bd74fd6260e19
duman <duman200354@mail.ru>
2021-12-21 12:11:01 +0600

    updated nav.style&js

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -2,20 +2,21 @@ Vue.component('navigation', {
     template: `
         <div>
         <div class="container">
-        <div class="header">
-            <div class="wrapper">
-                <div>
-                    <a href=""><img class="logo" src="./image/navigation/SpaceX-Logo.svg"></img>
-                </div>
-                <div class="items">
-                    <a href="" v-for="nav in navs" class="item-text capital hover">{{ nav.item }}</a>
-                </div>
-                <div class="sign-up">
-                    <a href="" class="button_sign-up capital hover">sign up</a>
+            <div class="header">
+                <div class="wrapper">
+                    <div>
+                        <a href=""><img class="logo" src="./image/navigation/SpaceX-Logo.svg"></img>
+                    </div>
+                    <div class="items">
+                        <a href="" v-for="nav in navs" class="item-text capital hover">{{ nav.item }}</a>
+                    </div>
+                    <div class="sign-up">
+                        <a href="" class="button_sign-up capital hover">sign up</a>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>         
+            
+        </div>         
         </div>`
     ,
     data() {
