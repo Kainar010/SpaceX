7676ad02f24c90505e4ab32005975e2b95b98f98
Kainar010 <kainar0215@mail.ru>
2021-12-24 23:19:42 +0600

    asd

---
+++ javascript/rideshare/main.js
@@ -0,0 +1,66 @@
+Vue.component('navigation', {
+    template: `
+        <div>
+        <div class="container">
+        <div class="header">
+            <div class="wrapper">
+                <div>
+                    <a href=""><img class="logo" src="./image/navigation/SpaceX-Logo.svg"></img>
+                </div>
+                <div class="items">
+                    <a href="" v-for="nav in navs" class="item-text capital hover">{{ nav.item }}</a>
+                </div>
+                <div class="sign-up">
+                    <a href="" class="button_sign-up capital hover">sign up</a>
+                </div>
+            </div>
+        </div>
+    </div>         
+        </div>`
+    ,
+    data() {
+        return {
+            navs: [
+                {item: 'falcon 9'},
+                {item: 'falcan heavy'},
+                {item: 'dragon'},
+                {item: 'starship'},
+                {item: 'moon'},
+                {item: 'mars'}
+            ]
+        }
+    },
+})
+
+new Vue({
+    el: '#main',
+    data: {
+        mass: 0,
+        price: 0
+    },
+    method: {
+        calculate: function(){
+            this.price = this.mass * 11.28;
+        }
+    }
+});
+
+const nav = document.querySelector('.header');
+let prevScrollpos = window.pageYOffset;
+
+window.addEventListener('scroll', ()=>{
+    let currentScrollPos = window.pageYOffset;
+
+    if(prevScrollpos < currentScrollPos){
+        nav.classList.add('hide');
+    }else{
+        nav.classList.remove('hide');
+    }
+    if(currentScrollPos > window.innerHeight) {
+        nav.classList.add('back-color');
+    } else {
+        nav.classList.remove('back-color');
+    }
+
+    prevScrollpos = currentScrollPos;
+})
